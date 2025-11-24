c5b6c4c5d0fab1f83c9bda804eb3112c867f68ee
Daniel Bereznyi <daniel@giwn.xyz>
2025-11-24 12:02:54 +0900

    remove unused timer state

--- src/gameboy/timer/timer.zig
+++ src/gameboy/timer/timer.zig
@@ -10,7 +10,6 @@ pub const Timer = struct {
     };
 
     system_counter: u16,
-    cycles_elapsed: usize,
     state: State,
 
     odd_cycles: usize,
@@ -18,27 +17,24 @@ pub const Timer = struct {
     pub fn init() Self {
         return Self{
             .system_counter = 0,
-            .cycles_elapsed = 0,
             .state = .running,
             .odd_cycles = 0,
         };
     }
 
     pub fn printState(timer: *const Self, writer: *std.Io.Writer) !void {
-        try writer.print("state={s} cycles_elapsed={} system_counter={x:0>4}\n", .{
+        try writer.print("state={s} system_counter={x:0>4}\n", .{
             switch (timer.state) {
                 .running => "running",
                 .reloading_tima => "reloading_tima",
                 .reloaded_tima => "reloaded_tima",
             },
-            timer.cycles_elapsed,
             timer.system_counter,
         });
     }
 
     pub fn reset(self: *Self) void {
         self.system_counter = 0;
-        self.cycles_elapsed = 0;
         self.state = .running;
         self.odd_cycles = 0;
     }
